8cf6120d6fdf23ba3e8c5bb0d79641334df78d92
mike <mapeacock86@gmail.com>
2022-06-06 23:48:04 -0400

    search api init

--- api/api.go
+++ api/api.go
@@ -2,10 +2,39 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 
-	"rsc.io/quote/v4"
+	"github.com/gorilla/mux"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	err := godotenv.Load()
+	omdb_key := os.Getenv("OMDB_API_KEY")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	r := mux.NewRouter()
+	r.HandleFunc("/search/{title}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		res, err := http.Get("https://www.omdbapi.com/?s=" + vars["title"] + "&page=1&type=movie&apikey=" + omdb_key)
+		if err != nil {
+			fmt.Print("Error", w)
+		}
+		respBody, _ := ioutil.ReadAll(res.Body)
+
+		w.Write(respBody)
+	})
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	// })
+
+	// "https://www.omdbapi.com/?s="+title+"&page=1&type=movie&apikey=1511e143"
+
+	http.ListenAndServe(":80", r)
 }
--- api/go.mod
+++ api/go.mod
@@ -2,9 +2,7 @@ module api
 
 go 1.18
 
-require rsc.io/quote/v4 v4.0.1
-
 require (
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
-	rsc.io/sampler v1.3.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
 )
