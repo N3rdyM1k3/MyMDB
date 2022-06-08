7283a24ddc17b8652229c1fb0aba981b034a3af0
mike <mapeacock86@gmail.com>
2022-06-08 00:09:05 -0400

    made http request async

--- api/api.go
+++ api/api.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -14,27 +13,26 @@ import (
 
 func main() {
 	err := godotenv.Load()
-	omdb_key := os.Getenv("OMDB_API_KEY")
 
 	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
 	r := mux.NewRouter()
 	r.HandleFunc("/search/{title}", func(w http.ResponseWriter, r *http.Request) {
+		omdbChan := make(chan *http.Response)
 		vars := mux.Vars(r)
-		res, err := http.Get("https://www.omdbapi.com/?s=" + vars["title"] + "&page=1&type=movie&apikey=" + omdb_key)
-		if err != nil {
-			fmt.Print("Error", w)
-		}
+		title := vars["title"]
+		go callOmdb(title, omdbChan)
+		res := <-omdbChan
 		respBody, _ := ioutil.ReadAll(res.Body)
 
 		w.Write(respBody)
 	})
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	// })
-
-	// "https://www.omdbapi.com/?s="+title+"&page=1&type=movie&apikey=1511e143"
-
 	http.ListenAndServe(":80", r)
+}
+
+func callOmdb(title string, c chan *http.Response) {
+	omdb_key := os.Getenv("OMDB_API_KEY")
+	res, _ := http.Get("https://www.omdbapi.com/?s=" + title + "&page=1&type=movie&apikey=" + omdb_key)
+	c <- res
 }
