a08e40ab5047bd1348feb35b8bdc64f7a4105341
mike <mapeacock86@gmail.com>
2022-07-07 22:12:30 -0400

    get all movies works

--- api/handlers/get.go
+++ api/handlers/get.go
@@ -1,12 +1,9 @@
 package handlers
 
 import (
+	"api/repositories"
+	"encoding/json"
 	"net/http"
-	"os"
-	"log"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // https://stackoverflow.com/questions/31622052/how-to-serve-up-a-json-response-using-go
@@ -14,32 +11,16 @@ type Payload struct {
 	data []interface{}
 }
 
-
-func HandleGetAll(w http.ResponseWriter, r *http.Request){
-	responseChannel := make(chan *http.Response)
-
+func HandleGetAll(w http.ResponseWriter, r *http.Request) {
+	dataChan := make(chan Payload)
+	go getAllMovies(dataChan)
+	movies := <-dataChan
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies.data)
 }
 
-func getAllMovies(responseChannel chan *http.Response){
-	mongo_key := os.Getenv("MONGO_PWD")
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://mymdb:" + mongo_key + "@mymdb.fscoq.mongodb.net/?retryWrites=true&w=majority"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, _ context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
-	collection := client.Database("mymdb").Collection("movies")
-	var movies []interface{}
-	err := collection.Find().Decode(&movies)
-	if err != nil {
-		log.Fatal(err)
-	}
-	res := http.Response
-	res.
-	responseChannel <- res
-	
+func getAllMovies(c chan Payload) {
+	movies := repositories.GetMovies()
+	p := Payload{movies}
+	c <- p
 }
