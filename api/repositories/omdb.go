3246acff192d8e5c9f21fd8399738c546be38412
mike <mapeacock86@gmail.com>
2022-12-24 13:17:34 -0500

    its working

--- api/repositories/omdb.go
+++ api/repositories/omdb.go
@@ -11,8 +11,6 @@ func GetOmdbMovies(title string, c chan MovieCollection) {
 	res, _ := http.Get("https://www.omdbapi.com/?s=" + title + "&page=1&type=movie&apikey=" + omdb_key)
 	var search SearchResponse
 	defer res.Body.Close()
-	// body, _ := ioutil.ReadAll(res.Body)
-	// fmt.Print(string(body))
 	json.NewDecoder(res.Body).Decode(&search)
 	m := MovieCollection{search.Search}
 	c <- m
