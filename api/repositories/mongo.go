949ca1e62108a40d0be5c22d603ba1202a5b129e
Mike Peacock <MAPeacock86@gmail.com>
2022-12-24 16:00:58 -0500

    fixed bug

--- api/repositories/mongo.go
+++ api/repositories/mongo.go
@@ -78,8 +78,7 @@ func buildClient() (*mongo.Client, context.Context) {
 		log.Fatal(err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
