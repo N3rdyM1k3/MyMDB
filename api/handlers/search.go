923fe4d7bf210dae7bfcb36151dee1034571f8d4
Mike Peacock <MAPeacock86@gmail.com>
2025-08-15 17:17:19 -0400

    switched search api to return omdb

--- api/handlers/search.go
+++ api/handlers/search.go
@@ -10,7 +10,7 @@ import (
 func HandleSearch(c *gin.Context) {
 	title := c.Param("title")
 	omdbChan := make(chan repositories.MovieCollection)
-	go repositories.SearchOwnedMovies(title, omdbChan)
+	go repositories.SearchOmdb(title, omdbChan)
 	m := <-omdbChan
 	c.IndentedJSON(http.StatusOK, m.Movies)
 }
